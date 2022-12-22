57a49e20d31ea4b940cb2143087156c6a02dffdc
Kvezac <Kvezak@gmail.com>
2022-12-23 01:25:32 +0300

    Create the project, registered the path.

---
+++ src/js/classroom.js
@@ -0,0 +1,31 @@
+/* classroom.js */
+
+/** @param {number[]} grades */
+export function getNumberOfGrades(grades) {
+	// your code
+}
+
+/** @param {number[]} grades */
+export function getSumGrades(grades) {
+  // your code
+}
+
+/** @param {number[]} grades */
+export function getAverageValue(grades) {
+  // your code
+}
+
+/** @param {number[]} grades */
+export function getPassingGrades(grades) {
+  // your code
+}
+
+/** @param {number[]} grades */
+export function getFailingGrades(grades) {
+  // your code
+}
+
+/** @param {number[]} grades */
+export function getRaisedGrades(grades) {
+  // your code
+}
